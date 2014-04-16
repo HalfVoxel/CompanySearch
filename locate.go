a0cd673947fbfcf07816e398702d7e915f4c0b38
Aron Granberg <aron.granberg@gmail.com>
2014-04-16 16:03:42 +0200

    Search improvements.

--- locate.go
+++ locate.go
@@ -3,6 +3,114 @@ package main
 import "fmt"
 import "encoding/csv"
 import "os"
+import "strconv"
+import "bufio"
+import "log"
+import "flag"
 
-func main () {
-	
+var seenBefore map[int]bool = map[int]bool{}
+
+func Parse(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(file)
+	r.FieldsPerRecord = 9
+	r.TrailingComma = true
+	r.LazyQuotes = true
+	lines, err := r.ReadAll()
+
+	file.Close()
+	if err != nil {
+		log.Fatalf("error in file %v, %v", path, err)
+	}
+
+	for _, line := range lines {
+		index, _ := strconv.Atoi(line[2])
+
+		if seenBefore[index] {
+			continue
+		} else {
+			seenBefore[index] = true
+		}
+
+		fmt.Println(index)
+	}
+}
+
+func Find(path string, needleIndex int) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(file)
+	r.FieldsPerRecord = 9
+	r.TrailingComma = true
+	r.LazyQuotes = true
+	lines, err := r.ReadAll()
+
+	file.Close()
+	if err != nil {
+		log.Fatalf("error in file %v, %v", path, err)
+	}
+
+	for _, line := range lines {
+		index, _ := strconv.Atoi(line[2])
+
+		if index == needleIndex {
+			fmt.Printf("\"%s\",\"%s\",\"%s\", \"%s\"\n", line[1], line[4], line[5], line[8])
+		}
+	}
+}
+
+func main() {
+	const aleph = "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"
+	var generate = flag.Bool("generate", false, "generate cache file")
+
+	flag.Parse()
+
+	if *generate {
+
+		for _, c := range aleph {
+			for i := 0; i < 5000; i += 100 {
+
+				path := fmt.Sprintf("output/%c_%d.csv", c, i)
+				fmt.Println(path)
+				Parse(path)
+				fmt.Println("-1")
+			}
+		}
+
+	} else {
+
+		file, err := os.Open("lookupcache")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lookup := map[int]string{}
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			path := scanner.Text()
+			for scanner.Scan() {
+				s := scanner.Text()
+				index, _ := strconv.Atoi(s)
+				if index == -1 {
+					break
+				}
+				lookup[index] = path
+			}
+		}
+
+		scanner = bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			idx, _ := strconv.Atoi(scanner.Text())
+			//fmt.Println(lookup[idx])
+			Find(lookup[idx], idx)
+		}
+	}
+}
--- search.go
+++ search.go
@@ -16,27 +16,28 @@ import "net/http"
 import "strings"
 
 type Node struct {
-	lookup map[rune]*Node
-	links []Info
+	lookup    map[rune]*Node
+	links     []Info
 	lastQuery int
-	mins map[int]int
+	mins      map[int]int
 }
 
 type Info struct {
+	idx int
 	//name string
 }
 
 var counter int = 0
 var seenBefore map[int]bool = map[int]bool{}
 
-func (node *Node) Insert ( s string, index int ) {
+func (node *Node) Insert(s string, index int, idx int ) {
 
 	if index >= len(s) {
-		info := Info{}
+		info := Info{idx}
 		node.links = append(node.links, info)
 
-		if counter % 10000 == 0 {
-			fmt.Print("*")
+		if counter%10000 == 0 {
+			//fmt.Print("*")
 		}
 
 		counter++
@@ -49,17 +50,18 @@ func (node *Node) Insert ( s string, index int ) {
 	if !ok {
 		next = new(Node)
 		//next.val = v
-		next.lookup = map[rune]*Node {}
+		next.lookup = map[rune]*Node{}
 		node.lookup[v] = next
 	}
 
 	//fmt.Print(string(v))
-	next.Insert (s,index+len)	
+	next.Insert(s, index+len, idx)
 }
 
 type Result struct {
 	result string
-	error int
+	error  int
+	idx int
 }
 
 type ByError []Result
@@ -68,13 +70,13 @@ func (a ByError) Len() int           { return len(a) }
 func (a ByError) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByError) Less(i, j int) bool { return a[i].error < a[j].error }
 
-func (node *Node) Search ( s string, index int, trace string, skipped int, queryIndex int, result *[]Result, best *int) (int) {
-	
+func (node *Node) Search(s string, index int, trace string, skipped int, queryIndex int, result *[]Result, best *int) int {
+
 	if node.lastQuery != queryIndex {
 		node.lastQuery = queryIndex
-		node.mins = map[int]int {index: skipped}
+		node.mins = map[int]int{index: skipped}
 	} else {
-		prevErr, ok2 :=  node.mins[index]
+		prevErr, ok2 := node.mins[index]
 		//fmt.Printf("Been here before? %v %d < %d", ok2, prevErr, skipped)
 		if ok2 && prevErr <= skipped {
 			return -1
@@ -82,140 +84,141 @@ func (node *Node) Search ( s string, index int, trace string, skipped int, query
 
 		node.mins[index] = skipped
 	}
-	
-		if skipped > *best+6 {
-			return -1
-		}
 
-		var v rune
-		length := 0
-
-		if index >= len(s) {
-			/*for i, _ := range node.links {
-				_ = i
-				fmt.Printf("Found %s with error %d #%d\n", trace, skipped,i)
-			}*/
-
-			if len(node.links) > 0 {
-				*result = append(*result, Result{trace,skipped})
-			
-				if skipped < *best {
-					*best = skipped
-				}
-			}
-			//return 0
-		} else {
-
-			v, length = utf8.DecodeRuneInString(s[index:])
-
-			//fmt.Print(string(v))
-
-			next, ok := node.lookup[v]
-			if ok {
-				next.Search (s, index+length, trace +string(v), skipped+0, queryIndex, result, best)
-			}
-		}
-
-		if skipped < 16 {
-			// Skip
-			node.Search (s, index+length, trace, skipped+2, queryIndex, result, best)
-
-			for k, other := range node.lookup {
-				if k != v {
-					// Insert
-					other.Search (s, index, trace +string(k), skipped+1, queryIndex, result, best)
-					// Replace
-					other.Search (s, index+length, trace +string(k), skipped+3, queryIndex, result, best)
-				}
-			}
-		}
-
+	if skipped > *best+6 {
 		return -1
 	}
 
-	func parse (root *Node, path string ) {
-		file, err := os.Open(path)
-		if err != nil {
-			log.Fatal (err)
-		}
+	var v rune
+	length := 0
 
-		r := csv.NewReader (file)
-		r.FieldsPerRecord = 9
-		r.TrailingComma = true
-		r.LazyQuotes = true
-		lines, err := r.ReadAll ()
+	if index >= len(s) {
+		for _, v := range node.links {
+			//if len(node.links) > 0 {
+			*result = append(*result, Result{trace, skipped, v.idx})
 
-		file.Close ()
-		if err != nil {
-			log.Fatalf ("error in file %v, %v", path, err)
-		}
-
-		for _, line := range lines {
-			index, _ := strconv.Atoi (line[2])
-			
-			if seenBefore[index] {
-				continue
-			} else {
-				seenBefore[index] = true
+			if skipped < *best {
+				*best = skipped
 			}
+			//}
+			//fmt.Printf("Found %s with error %d #%d\n", trace, skipped,i)
+		}
 
-			root.Insert (line[1], 0)
-			/*for i, _ := range line[1] {
-				fmt.Printf (line[1][i])//"%v",runeValue)
-			}*/
+		
+		//return 0
+	} else {
+
+		v, length = utf8.DecodeRuneInString(s[index:])
+
+		//fmt.Print(string(v))
+
+		next, ok := node.lookup[v]
+		if ok {
+			next.Search(s, index+length, trace+string(v), skipped+0, queryIndex, result, best)
 		}
 	}
 
-	func main() {
-		const aleph = "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"
+	if skipped < 16 {
+		// Skip
+		node.Search(s, index+length, trace, skipped+2, queryIndex, result, best)
 
-		var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+		for k, other := range node.lookup {
+			if k != v {
+				// Insert
+				other.Search(s, index, trace+string(k), skipped+1, queryIndex, result, best)
+				// Replace
+				other.Search(s, index+length, trace+string(k), skipped+3, queryIndex, result, best)
+			}
+		}
+	}
 
-		flag.Parse()
-	    if *cpuprofile != "" {
+	return -1
+}
+
+func parse(root *Node, path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(file)
+	r.FieldsPerRecord = 9
+	r.TrailingComma = true
+	r.LazyQuotes = true
+	lines, err := r.ReadAll()
+
+	file.Close()
+	if err != nil {
+		log.Fatalf("error in file %v, %v", path, err)
+	}
+
+	for _, line := range lines {
+		index, _ := strconv.Atoi(line[2])
+
+		if seenBefore[index] {
+			continue
+		} else {
+			seenBefore[index] = true
+		}
+
+		root.Insert(line[1], 0, index)
+		/*for i, _ := range line[1] {
+			fmt.Printf (line[1][i])//"%v",runeValue)
+		}*/
+	}
+}
+
+func main() {
+	const aleph = "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"
+
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+	flag.Parse()
+	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
 		if err != nil {
-		    log.Fatal(err)
+			log.Fatal(err)
 		}
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
-	    }
+	}
 
-	    go func() {
-			log.Println(http.ListenAndServe("localhost:6060", nil))
-		}()
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 
-		node := new(Node)
-		node.lookup = map[rune]*Node {}
+	node := new(Node)
+	node.lookup = map[rune]*Node{}
 
-		for _,c := range aleph {
-			for i := 0; i < 5000; i += 100 {
-				path := fmt.Sprintf("output/%c_%d.csv",c,i)
-				parse (node, path)
-			}
+	for _, c := range aleph {
+		for i := 0; i < 5000; i += 100 {
+			path := fmt.Sprintf("output/%c_%d.csv", c, i)
+			parse(node, path)
+		}
+	}
+
+	fmt.Println("")
+
+	bio := bufio.NewReader(os.Stdin)
+	cnt := 0
+	for {
+		cnt++
+
+		needle, _, err := bio.ReadLine()
+		sneedle := strings.ToLower(string(needle))
+		if err != nil {
+			return
+			//panic(err)
 		}
 
-		fmt.Println("")
+		if sneedle == "x" {
+			return
+		}
 
-		bio := bufio.NewReader(os.Stdin)
-		cnt := 0
-		for {
-			cnt++
-
-			needle, _, err := bio.ReadLine()
-			sneedle := strings.ToLower(string(needle))
-			if err != nil {
-				panic(err)
-			}
-
-			if sneedle == "x" {
-				return
-			}
-
-			fmt.Printf("Searching for %s\n",needle)
-			var res []Result
-			best := 1000
-		node.Search(sneedle,0,"",0,cnt, &res, &best)
+		//fmt.Printf("Searching for %s\n",needle)
+		var res []Result
+		best := 1000
+		node.Search(sneedle, 0, "", 0, cnt, &res, &best)
 
 		sort.Sort(ByError(res))
 
@@ -223,8 +226,9 @@ func (node *Node) Search ( s string, index int, trace string, skipped int, query
 			if i > 10 {
 				break
 			}
-			fmt.Printf("Found Result: %d: %s\n", v.error, v.result)
+			fmt.Printf("%d,%d", v.error, v.idx)
+			//fmt.Printf("Found Result: %d: %s\n", v.error, v.result)
 		}
-		fmt.Println("Done")
+		//fmt.Println("Done")
 	}
 }
